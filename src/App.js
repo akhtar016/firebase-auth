8d34f6eeb7ebdd2608498452abfc470eeb5ad611
theDreamerNYM <akhtaruzzamankhan016@gmail.com>
2020-03-17 18:14:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+  const handleSignIn = () => {
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+
+
+      const {displayName, photoURL, email} = result.user;
+      console.log(displayName, email, photoURL);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      //var token = result.credential.accessToken;
+      // The signed-in user info.
+      //var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+     // var errorCode = error.code;
+     // var errorMessage = error.message;
+      // The email of the user's account used.
+     // var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+     // var credential = error.credential;
+      // ...
+    });
+    
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <button onClick={handleSignIn}>Sign in</button>
+      
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0-A_HPdu71pAhPK4Wg6vjmALt7SioZew",
+    authDomain: "ema-john-simple-34463.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-34463.firebaseio.com",
+    projectId: "ema-john-simple-34463",
+    storageBucket: "ema-john-simple-34463.appspot.com",
+    messagingSenderId: "9594485988",
+    appId: "1:9594485988:web:9e50794c4080b6897280f1"
+  };
+
+  export default firebaseConfig;
